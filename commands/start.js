77925ca9d9a5a053c2881eaa82f6ac2bf7da570f
Sory Ronald <165383634+SoryRonald@users.noreply.github.com>
2025-01-01 22:50:48 +0000

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -2,7 +2,7 @@ module.exports = (bot) => {
   bot.command('start', (ctx) => {
     const userName = ctx.from.first_name; 
 
-    const welcomeMessage = `👋 Bonjour ${userName} \n\nBienvenue chère utilisateur ou utilisatrice moi c'est Ronald Chatbot \n\n- /help consultez la liste de mes commandes \n\n veuillez contacter @Alexandre_Ronald_SORY si le bot à un problème 🤗`;
+    const welcomeMessage = `👋 Bonjour ${userName} \n\nBienvenue chère utilisateur ou utilisatrice moi c'est Ronald Chatbot \n\n- /help consultez la liste de mes commandes \n\n veuillez contacter @RonaldAlexandreSory si le bot à un problème 🤗`;
 
     ctx.reply(welcomeMessage);
   });
