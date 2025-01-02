561ca22be32f68c5ab8869241646a5069dd1373e
gandxo-tech <gbaguidiexauce@gmail.com>
2025-01-02 20:49:18 +0100

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -5,14 +5,14 @@ const bot = new Telegraf('7871672665:AAGfaBgyN9FYB93gBvneXlyaB1Spk0d6b5I'); // R
 const apiKey = 'd8e6a31f799fadb31b6ed4bec3335088'; // Remplacez par votre clé API OpenWeatherMap
 
 // Exemple de commande météo
-bot.command('meteo', async (ctx) => {
+bot.command('weather', async (ctx) => {
     const city = ctx.message.text.split(' ')[1]; // Récupère le nom de la ville
     if (!city) {
-        return ctx.reply('Veuillez spécifier une ville. Exemple : /meteo Paris');
+        return ctx.reply('Veuillez spécifier une ville. Exemple : /weather Paris');
     }
 
     try {
-        const response = await axios.get(https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric);
+        const response = await axios.get(https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${d8e6a31f799fadb31b6ed4bec3335088}&units=metric);
         const weatherData = response.data;
         const message = Météo à ${weatherData.name} : ${weatherData.weather[0].description}, Température : ${weatherData.main.temp}°C;
         ctx.reply(message);
